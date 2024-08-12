e6c2201ab491b10729c493684dbc7b4237c63c0f
Ri-O15 <randiaprianto017@gmail.com>
2024-08-12 08:25:41 +0700

    delete isi

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -6,7 +6,7 @@
             <div
                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                 <img src="img/AssetSenjani/11.Snack.jpg"
-                    alt="Front of men's Basic Tee in black."
+                    alt="Front of men's Basic in black."
                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
             </div>
             <div class="mt-4 flex justify-between">
@@ -15,7 +15,7 @@
                         <a href="#">
                             <span aria-hidden="true"
                                 class="absolute inset-0"></span>
-                            Basic Tee
+                            Basic
                         </a>
                     </h3>
                     <p class="mt-1 text-sm text-gray-500">Black</p>
@@ -30,7 +30,7 @@
             <div
                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                 <img src="img/AssetSenjani/1.Tanpa.jpg"
-                    alt="Front of men's Basic Tee in black."
+                    alt="Front of men's Basic in black."
                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
             </div>
             <div class="mt-4 flex justify-between">
@@ -39,7 +39,7 @@
                         <a href="#">
                             <span aria-hidden="true"
                                 class="absolute inset-0"></span>
-                            Basic Tee
+                            Basic
                         </a>
                     </h3>
                     <p class="mt-1 text-sm text-gray-500">Black</p>
@@ -54,7 +54,7 @@
             <div
                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                 <img src="img/AssetSenjani/3.Merah.jpg"
-                    alt="Front of men's Basic Tee in black."
+                    alt="Front of men's Basic in black."
                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
             </div>
             <div class="mt-4 flex justify-between">
@@ -63,7 +63,7 @@
                         <a href="#">
                             <span aria-hidden="true"
                                 class="absolute inset-0"></span>
-                            Basic Tee
+                            Basic
                         </a>
                     </h3>
                     <p class="mt-1 text-sm text-gray-500">Black</p>
@@ -78,7 +78,7 @@
             <div
                 class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-90">
                 <img src="img/AssetSenjani/2.Putih.jpg"
-                    alt="Front of men's Basic Tee in black."
+                    alt="Front of men's Basic in black."
                     class="h-full w-full object-cover object-center lg:h-full lg:w-full">
             </div>
             <div class="mt-4 flex justify-between">
@@ -87,7 +87,7 @@
                         <a href="#">
                             <span aria-hidden="true"
                                 class="absolute inset-0"></span>
-                            Basic Tee
+                            Basic
                         </a>
                     </h3>
                     <p class="mt-1 text-sm text-gray-500">Black</p>
